5808a2d703244c962965e54b1e8bd44004826676
micabressan <micabressan@gmail.com>
2015-07-03 23:24:34 -0300

    Cambios de nombres

--- spider/spider/items.py
+++ spider/spider/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class TrabajoFinalItem(scrapy.Item):
+class FinalWorkItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     title = scrapy.Field()
