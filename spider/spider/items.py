72b527bf410c8fb51ddfc9e69a46246a58fd13dd
micabressan <micabressan@gmail.com>
2015-07-05 23:35:31 -0300

    agregamos starts urls

--- spider/spider/items.py
+++ spider/spider/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class FinalWorkItem(scrapy.Item):
+class TrabajoFinalItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     title = scrapy.Field()
