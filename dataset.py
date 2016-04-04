b2149195a70f9ca46eaf1b2582aef4056f5eb040
mica <micabressan@gmail.com>
2016-04-04 19:04:14 -0300

    script para armado de dataset y requirements

---
+++ dataset.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+path = '/home/mica/trabajo_final/codes/'
+files_list = []
+dir_list = os.walk(path)
+
+for root, dirs, files in dir_list:
+    for thing in files:
+        (name, extension) = os.path.splitext(thing)
+        files_list.append(name + extension)
+
+dataset = []
+
+for files in files_list:
+    code = open('/home/mica/trabajo_final/codes/' + files, "r")
+    languaje = files.split(".")[-1]
+    content = code.read()
+    dataset.append((content, languaje))
+
+
