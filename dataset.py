665d659eb31bb69089895c8acb3f4c0e631e6173
mica <micabressan@gmail.com>
2016-04-18 19:35:16 -0300

    empezamos con countvectorizer

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 path = '/home/mica/trabajo_final/codes/'
@@ -13,11 +14,31 @@ for root, dirs, files in dir_list:
         files_list.append(name + extension)
 
 dataset = []
+separado = []
 
 for files in files_list:
     code = open('/home/mica/trabajo_final/codes/' + files, "r")
     languaje = files.split(".")[-1]
     content = code.read()
-    dataset.append((content, languaje))
+    dataset.append([content, languaje])
+    #ver desde aca
+    lines = code.readline()
+    temporal = []
+    while lines != "":
+        temporal.append(content)
+        lines = code.readline()
+print dataset[0][0]
+print separado
+
+
+vectorizer = CountVectorizer(dataset[0][0])
+tf = vectorizer.fit_transform(separado).toarray()
+lista = []
+lista.append([separado[0], int(tf[0])])
+print vectorizer.input
+print tf
+print lista
+
+
 
 
