3a31c6de22506925e386284a029036a5210ab7a9
alexis <alexiswalker@hotmail.com>
2017-12-09 12:07:57 -0300

    preprocesor

--- clasificador_ale.py
+++ clasificador_ale.py
@@ -1,4 +1,6 @@
-from util import load_data
+from util import load_data, load_data_language
+
+from sklearn.pipeline import Pipeline
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -10,35 +12,92 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.decomposition import TruncatedSVD
 from sklearn.svm import LinearSVC
 
-from sklearn.metrics import accuracy_score
+from sklearn import metrics
 
-import numpy as np
+import re
+from keyword import kwlist
+
+def tokenizer(text):
+    r = []
+    for i in text.split('\n'):
+        for j in i.split(' '):
+            r.append(j)
+    rr = []
+    for i in r:
+        if i in kwlist:
+            rr.append(i)
+        else:
+            rr.append('Null')
+
+    return rr
+
+def preprocessor(text):
+    text = re.sub('\d+', 'NUMBER',
+                  re.sub('\".*?\"', 'STRING',
+                         re.sub('\'.*?\'', 'STRING', text)))
+    text = re.sub('\#.*', 'COMMENT', text)
+    text = re.sub('\//.*', 'COMMENT', text)
+
+    return text
+
+#http://scikit-learn.org/stable/modules/feature_extraction.html
 
 if __name__ == '__main__':
-    X, y = load_data('codes')
+    X, y = load_data_language('codes', ['Python', 'Java', 'C'])
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    count_vect = CountVectorizer()
-    tfidf_transformer = TfidfTransformer()
+    text_clf = Pipeline(
+        [
+            ('vect', CountVectorizer(preprocessor=preprocessor)),
+            ('tfidf', TfidfTransformer(use_idf=False)),
+            ('clf', SGDClassifier(
+                loss='hinge',
+                penalty='l2',
+                alpha=1e-3,
+                random_state=42,
+                max_iter=5,
+                tol=None)),])
+
+    text_clf.fit(X_train, y_train)
+    predicted = text_clf.predict(X_test)
+    print(metrics.accuracy_score(predicted, y_test))
+    print metrics.classification_report(y_test, predicted)
+    print metrics.confusion_matrix(y_test, predicted)
+
+
+
+    '''
+    count_vect = CountVectorizer(tokenizer=tokenizer)
+
+    #tfidf_transformer = TfidfTransformer()
 
     X_train_counts = count_vect.fit_transform(X_train)
-    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+    #X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
     X_test_counts = count_vect.transform(X_test)
-    X_test_tfidf = tfidf_transformer.transform(X_test_counts)
-
+    #X_test_tfidf = tfidf_transformer.transform(X_test_counts)
 
     clf = MultinomialNB().fit(X_train_tfidf, y_train)
     predicted = clf.predict(X_test_tfidf)
-    print np.mean(predicted == y_test)
-    print(accuracy_score(predicted, y_test))
+    print(metrics.accuracy_score(predicted, y_test))
 
-    clf = LinearSVC().fit(X_train_tfidf, y_train)
-    predicted = clf.predict(X_test_tfidf)
-    print np.mean(predicted == y_test)
-    print(accuracy_score(predicted, y_test))
+    clf = LinearSVC().fit(X_train_counts, y_train)
+    predicted = clf.predict(X_test_counts)
+    print(metrics.accuracy_score(predicted, y_test))
 
-    clf = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None).fit(X_train_tfidf, y_train)
-    predicted = clf.predict(X_test_tfidf)
-    print(accuracy_score(predicted, y_test))
+    #print metrics.classification_report(y_test, predicted)
+    #print metrics.confusion_matrix(y_test, predicted)
+
+
+    clf = SGDClassifier(
+        loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None
+        ).fit(X_train_counts, y_train)
+
+    predicted = clf.predict(X_test_counts)
+    print(metrics.accuracy_score(predicted, y_test))
+
+    #print metrics.classification_report(y_test, predicted)
+    #print metrics.confusion_matrix(y_test, predicted)
+
+    '''
--- util.py
+++ util.py
@@ -15,13 +15,32 @@ def load_data(folder):
 
     return (X, y)
 
-def languajes(folder):
+def load_data_language(folder, languages):
+    "returns a (X, y) readed from codes folder"
+    X = []
+    y = []
+
+    for f in os.listdir(folder):
+        text = open(os.path.join(folder, f)).read()
+        syntax = f.split('.')[-1]
+
+        if syntax not in languages:
+            continue
+
+        X.append(text)
+        y.append(syntax.lower())
+
+    return (X, y)
+
+def languages(folder):
     return set([file_name.split('.')[-1] for file_name in os.listdir(folder)])
 
-def languajes_file_frecuency(folder):
-    for languaje in languajes(folder):
-        frecuency = len(fnmatch.filter(os.listdir(folder), '*.' + languaje))
-        print languaje, frecuency
+def languages_file_frecuency(folder):
+    for language in languages(folder):
+        frecuency = len(fnmatch.filter(os.listdir(folder), '*.' + language))
+        print language, frecuency
 
 if __name__ == '__main__':
-    print languajes_file_frecuency('codes')
+    print languages_file_frecuency('codes')
+
+    print 'abstract assert boolean break byte case catch char class const continue default do double else enum extends final finally float for goto if implements import instanceof int interface long native new package private protected public return short static strictfp super switch synchronized this throw throws transient try void volatile while'.split(' ')
