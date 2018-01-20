80f647a93cd8dde6f33038cf3062ab3695ee765b
alexis <alexiswalker@hotmail.com>
2018-01-20 16:24:55 -0300

    copy files

--- util.py
+++ util.py
@@ -1,5 +1,9 @@
 import os
 import fnmatch
+import re
+import shutil
+import uuid
+
 
 def load_data(folder):
     "returns a (X, y) readed from codes folder"
@@ -44,5 +48,49 @@ def languages_file_frecuency(folder, language=None):
         frecuency = len(fnmatch.filter(os.listdir(folder), '*.' + language))
         print language, frecuency
 
+def string(text):
+    comment_characters = ['\'', '"']
+    for character in comment_characters:
+        regular_expresion = character + '.*'
+        text = re.sub(regular_expresion, 'STRING', text)
+    return text
+
+def one_line_comment(text):
+    #agregar todos los caracteres de comentario
+    comment_characters = ['#', '//', '--', '\*', '%']
+    for character in comment_characters:
+        regular_expresion = character + '.*'
+        text = re.sub(regular_expresion, 'COMMENT', text)
+    return text
+
+def multiline_comment(text):
+    #agregar todos los pares de caracteres de comentario
+    comment_characters = [('/\*', '\*/'),('<!--', '-->')]
+    for begin, end in comment_characters:
+        regular_expresion = begin + '([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*' + end
+        text = re.sub(regular_expresion, 'COMMENT', text)
+    return text
+
+
+def move_files(origin_folder, destination_folder, language, extension):
+
+
+    destination = os.path.join(destination_folder, language)
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+
+    dir_list = os.walk(origin_folder)
+
+    files_to_move = [os.path.join(root,file) for root, dirs, files in dir_list for file in files if file.split('.')[-1]==extension]
+
+    for origin in files_to_move:
+        if os.stat(origin).st_size > 1024:
+            file_name = uuid.uuid4().hex + '.' + extension
+            shutil.move(origin, os.path.join(destination,file_name))
+
+
 if __name__ == '__main__':
-    languages_file_frecuency('codes')
+    move_files('/home/alexis/Descargas',
+               '/home/alexis/Escritorio/trabajo_final/language',
+               'Python',
+               'py')
