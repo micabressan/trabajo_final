ad994323e7abe8ae85caed188b96b1242cad801c
MicaBressan <micabressan@gmail.com>
2018-02-09 18:46:39 -0300

    arreglamos codigo

--- codes_prueba/kaGMc9Rw.Python
+++
@@ -1,5 +0,0 @@
-def main():
-    # my code here
-
-if __name__ == "__main__":
-    main()
--- codes_prueba/mtLMqiav.C
+++
@@ -1,6 +0,0 @@
-Encryption and decryption of text by bit rotation
-
-There are two encryption and decryption routines, contained in two files, encryption.c and decryption.c
-You can find:
-encryption.c  in http://past.is/ajgea   (Please copy this link and paste it on the address bar of your browser)
-decryption.c in http://past.is/ajgeb    (Please copy this link and paste it on the address bar of your browser)
